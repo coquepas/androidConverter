3a25fae21ac13ccb68c23ef5f2c9369b8cc2be07
Coque1994 <coquepas@gmail.com>
2024-01-03 14:12:55 +0100

    con https

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return redirect("http://www.google.es")
+    return redirect("https://www.google.es")
 
 if __name__=="__main__":
     app.run(debug=False,port=8080)
